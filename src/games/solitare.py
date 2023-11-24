016f1716594934f709c051eff4365923ffac2484
CruidGals <kchiem30@gmail.com>
2023-11-24 02:19:12 -0500

    Implement solitare game setup

--- src/games/solitare.py
+++ src/games/solitare.py
@@ -3,13 +3,29 @@ from card import Deck
 
 class Solitare:
     def __init__(self) -> None:
-        self.deck = Deck()
-        self.logic = SolitareGameLogic(self.deck.deck)
+        self.logic = SolitareGameLogic()
 
 
     def draw_elements(self):
         pass
 
+#Will include no pygame
 class SolitareGameLogic:
-    def __init__(self, card_deck) -> None:
-        self.deck = card_deck
+    def __init__(self) -> None:
+        self.deck = Deck()
+        self.setup_game()
+        
+    def setup_game(self):
+        self.tableau = [[], [], [], [], [], [], []] #Main playing area
+        self.foundation_piles = [[], [], [], []] #Piles that start with ace
+        self.stockpile = [[], []] #Includes talon pile (waste pile)
+
+        self.deck.shuffle_deck()
+        self.stockpile[0] = self.deck.tolist() #Do this so i can pop from the list
+
+        for i in range(1, 8): #Tableau col marked as i-1 (array indexing)
+            for j in range(i): #Range of i because of solitare shtife
+                card = self.stockpile[0].pop()
+                if j != i-1: card.front_shown = False
+
+                self.tableau[i-1].append(card)
