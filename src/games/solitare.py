d6ab43ed1405fc00f64397dcfbd92059e604100f
CruidGals <kchiem30@gmail.com>
2023-11-26 01:11:46 -0500

    Implement tableau swapping function

--- src/games/solitare.py
+++ src/games/solitare.py
@@ -1,5 +1,5 @@
 import pygame
-from card import Deck
+from card import Deck, PlayingCard
 
 class Solitare:
     def __init__(self) -> None:
@@ -29,3 +29,18 @@ class SolitareGameLogic:
                 if j != i-1: card.front_shown = False
 
                 self.tableau[i-1].append(card)
+
+    # In solitare, to shift from tableau piles you must satisfy these conditions:
+    #   1.) The card you are shifting is of opposite color to the card you are putting it on
+    #   2.) The card you are shifting must be a number one lower than the card you are putting it on
+    def swap_tableau_to_tableau(orig_pile: list, new_pile: list):
+
+        #Start out with a bunch of guard clauses
+        if(len(new_pile) != 0): #making sure the pile isn't empty before we do this
+            if(PlayingCard.is_same_color(orig_pile[-1], new_pile[-1])): return
+            if(orig_pile[-1].rank != new_pile[-1].rank - 1): return
+        else: #When a tableau pile is empty, you can only put a king on it
+            if(orig_pile[-1].rank != PlayingCard.KING): return
+
+        new_pile.append(orig_pile.pop())
+        orig_pile[-1].front_shown = True
