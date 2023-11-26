5653498b6a36abb5806d25c72927582bd0706293
CruidGals <kchiem30@gmail.com>
2023-11-26 01:34:19 -0500

    Fix so it can swap groups of cards

--- src/games/solitare.py
+++ src/games/solitare.py
@@ -32,21 +32,21 @@ class SolitareGameLogic:
                 self.tableau[i-1].append(card)
 
     # In solitare, to shift from tableau piles you must satisfy these conditions:
-    #   1.) The card you are shifting is of opposite color to the card you are putting it on
-    #   2.) The card you are shifting must be a number one lower than the card you are putting it on
-    def swap_tableau_to_tableau(self, orig_pile: list, new_pile: list):
+    #   1.) The cards you are shifting is of opposite color to the card you are putting it on
+    #   2.) The cards you are shifting must be a number one lower than the card you are putting it on
+    # Index exists if moving a group of cards (idx should be len(orig_pile) if just moving one card)
+    def swap_tableau_to_tableau(self, orig_pile: list, new_pile: list, idx):
 
         #Start out with a bunch of guard clauses
         if(len(new_pile) != 0): #making sure the pile isn't empty before we do this
-            if(orig_pile[-1].is_same_color(new_pile[-1])): return
-            if(orig_pile[-1].rank != new_pile[-1].rank - 1): return
+            if(orig_pile[idx].is_same_color(new_pile[-1])): return
+            if(orig_pile[idx].rank != new_pile[-1].rank - 1): return
         else: #When a tableau pile is empty, you can only put a king on it
-            if(orig_pile[-1].rank != PlayingCard.KING): return
+            if(orig_pile[idx].rank != PlayingCard.KING): return
 
-        new_pile.append(orig_pile.pop())
+        new_pile.append(orig_pile[idx:])
+        del orig_pile[idx:]
         orig_pile[-1].front_shown = True
-
-        #TODO make sure it works when you are moving a pile of cards onto another pile (ie a 3 and 2 stacked onto a 4)
 
     # Cards from stockpile fall into talon pile until there is no more cards
     # in stockpile, where which all the talon pile cards go back to stockpile.
